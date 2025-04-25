c6fade79a834ba74cd5494711dafe7307a643d6d
Roman Wernsing <wernsingroman@gmail.com>
2025-04-25 11:27:03 -0600

    added profile model and account inherits it

--- server/Models/Account.cs
+++ server/Models/Account.cs
@@ -1,9 +1,15 @@
 namespace keepr.Models;
 
-public class Account
+public class Account : Profile
+{
+  public string Email { get; set; }
+}
+
+public class Profile
 {
   public string Id { get; set; }
   public string Name { get; set; }
-  public string Email { get; set; }
   public string Picture { get; set; }
+  public string CoverImg { get; set; }
+
 }
